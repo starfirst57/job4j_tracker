b1dff59a1cc1c887ac7dc3026eba21bebbe4c87e
starfirst57@gmail.com <GmI514TwerK>
2021-08-18 14:50:17 +0700

    1.4. Вызов метода с возвращаемым типом. [#167387 #191214]

--- src/main/java/ru/job4j/oop/DummyDic.java
+++ src/main/java/ru/job4j/oop/DummyDic.java
@@ -4,4 +4,9 @@ public class DummyDic {
     public String engToRus(String eng) {
         return "Неизвестное слово. " + eng;
     }
+
+    public static void main(String[] args) {
+        DummyDic dictionary = new DummyDic();
+        System.out.println(dictionary.engToRus("hamburger"));
+    }
 }
