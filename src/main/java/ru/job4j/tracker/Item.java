4d0f2bd85aa9a87b79aee16f98430ff544175861
starfirst57@gmail.com <GmI514TwerK>
2021-08-20 14:35:15 +0700

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,22 +1,22 @@
 package ru.job4j.tracker;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 public class Item {
     private int id;
     private String name;
     private LocalDateTime created = LocalDateTime.now();
 
-    public Item() { }
+    public Item() {
+    }
 
     public Item(String name) {
         this.name = name;
     }
 
-    public Item(String name, int id) {
-        this.name = name;
+    public Item(int id, String name) {
         this.id = id;
+        this.name = name;
     }
 
     public int getId() {
--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -21,5 +23,33 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public Item[] findAll() {
+        Item[] notNull = new Item[items.length];
+        int size = 0;
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] != null) {
+                notNull[size] = items[i];
+                size++;
+            }
+        }
+        notNull = Arrays.copyOf(notNull, size);
+        return notNull;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] search = new Item[items.length];
+        int size = 0;
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] != null) {
+                if (key.equals(items[i].getName())) {
+                    search[size] = items[i];
+                    size++;
+                }
+            }
+        }
+        search = Arrays.copyOf(search, size);
+        return search;
     }
 }
