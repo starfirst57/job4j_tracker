2cfaa27884d5bad9df60cdafa9a9c2d9a4e369e3
starfirst <starfirst57@gmail.com>
2021-08-29 10:10:27 +0700

    2. Singleton [#94606]

---
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -0,0 +1,41 @@
+package ru.job4j.tracker;
+
+public final class SingleTracker {
+    private Tracker tracker = new Tracker();
+
+    private static SingleTracker singletrack = null;
+
+    public SingleTracker getSingletrack() {
+        if (singletrack == null) {
+            singletrack = new SingleTracker();
+        }
+        return singletrack;
+    }
+
+    private SingleTracker() {}
+
+    public Item add(Item item) {
+        return tracker.add(item);
+    }
+
+    public Item findById(int id) {
+        return null;
+    }
+
+    public Item[] findByName(String name){
+        return tracker.findByName(name);
+    }
+
+    public Item[] showAll() {
+       return tracker.findAll();
+    }
+
+    public void delete(int id) {
+        tracker.delete(id);
+    }
+
+    public boolean replace(int id, Item item) {
+        return tracker.replace(id, item);
+    }
+}
+
