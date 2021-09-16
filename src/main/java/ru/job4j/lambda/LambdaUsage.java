e83abbed29c733727e8e1e08585cbfc43eebd2da
starfirst57@gmail.com <GmI514TwerK>
2021-09-16 15:48:11 +0700

    2.2. Лямбда блок [#249208]

---
+++ src/main/java/ru/job4j/lambda/LambdaUsage.java
@@ -0,0 +1,19 @@
+package ru.job4j.lambda;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class LambdaUsage {
+    public static void main(String[] args) {
+        Comparator<String> comparator = (left, right) -> {
+            System.out.println("compare " + right.length() + " : " + left.length());
+            return right.length() - left.length();
+        };
+        List<String> list = new ArrayList();
+        list.add("abcde");
+        list.add("abc");
+        list.sort(comparator);
+        list.forEach(System.out::println);
+    }
+}
