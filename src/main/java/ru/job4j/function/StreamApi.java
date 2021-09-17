4a580e887cf180c6fd4cac9bee1ccc3ad4567ccb
starfirst57@gmail.com <GmI514TwerK>
2021-09-17 13:52:07 +0700

    0. Stream API [#58166]

---
+++ src/main/java/ru/job4j/function/StreamApi.java
@@ -0,0 +1,13 @@
+package ru.job4j.function;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamApi {
+    public static void main(String[] args) {
+        List<Integer> list = List.of(1, 2, 3, 4, 5, -6, 7, 8, -10);
+        List<Integer> positive = list.stream().filter(el -> el > 0)
+                .collect(Collectors.toList());
+        positive.forEach(System.out::println);
+    }
+}
