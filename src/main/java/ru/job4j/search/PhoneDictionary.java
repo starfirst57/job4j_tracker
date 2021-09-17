34d90efaad8ffaf97e8d53d0ed5237da3ac4c834
starfirst57@gmail.com <GmI514TwerK>
2021-09-17 11:59:18 +0700

    6. Функции высшего порядка [#24255]

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -1,21 +1,24 @@
 package ru.job4j.search;
 
 import java.util.ArrayList;
+import java.util.function.Predicate;
 
 public class PhoneDictionary {
-    private ArrayList<Person> persons = new ArrayList<Person>();
+    private ArrayList<Person> persons = new ArrayList<>();
 
     public void add(Person person) {
         this.persons.add(person);
     }
 
     public ArrayList<Person> find(String key) {
+        Predicate<Person> prName = (person) -> person.getName().contains(key);
+        Predicate<Person> prSurname = (person) -> person.getSurname().contains(key);
+        Predicate<Person> prPhone = (person) -> person.getPhone().contains(key);
+        Predicate<Person> prAddress = (person) -> person.getAddress().contains(key);
+        Predicate<Person> combine = (t) -> prName.or(prSurname.or(prPhone.or(prAddress))).test(t);
         ArrayList<Person> result = new ArrayList<>();
-        for (Person person: persons) {
-            if (person.getAddress().contains(key)
-                    || person.getName().contains(key)
-                    || person.getSurname().contains(key)
-                    || person.getPhone().contains(key)) {
+        for (Person person : persons) {
+            if (combine.test(person)) {
                 result.add(person);
             }
         }
