4b11e71f6576a54d7f050879c78aad537da1c7af
starfirst57@gmail.com <GmI514TwerK>
2021-09-20 11:32:43 +0700

    3. Уникальность элементов и сортировка. [#110225 #205539]

---
+++ src/main/java/ru/job4j/stream/Card.java
@@ -0,0 +1,27 @@
+package ru.job4j.stream;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+enum Suit {
+    Diamonds, Hearts, Spades, Clubs
+}
+
+enum Value {
+    V_6, V_7, V_8
+}
+
+public class Card {
+    private Suit suit;
+    private Value value;
+
+    public Card(Suit suit, Value value) {
+        this.suit = suit;
+        this.value = value;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -7,11 +7,9 @@ import java.util.stream.Collectors;
 
 public class Profiles {
     public static List<Address> collect(List<Profile> profiles) {
-        Comparator<Address> comparator = (left, right) ->
-                left.getCity().compareTo(right.getCity());
         return profiles.stream()
                 .map(Profile::getAddress)
-                .sorted(comparator)
+                .sorted(Comparator.comparing(Address::getCity))
                 .distinct()
                 .collect(Collectors.toList());
     }
